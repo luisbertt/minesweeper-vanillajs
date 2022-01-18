13b7b219d758b1849a44a1eda3388cea62d03fbd
luisbertt <luisbrttt@gmail.com>
2022-01-18 11:10:38 -0500

    set tabWidth to 4

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const boardElement = document.getElementById("board");
 const resetBtnElement = document.getElementById("reset");
 
 resetBtnElement.onclick = () => {
-  while (boardElement.firstChild) {
-    boardElement.removeChild(boardElement.firstChild);
-  }
-  resetBtnElement.textContent = "🙂";
-  board = generateBoard();
-  generateBombs(board);
-  addAdjacentBombsNumber(board);
-  createCovers(board);
+    while (boardElement.firstChild) {
+        boardElement.removeChild(boardElement.firstChild);
+    }
+    resetBtnElement.textContent = "🙂";
+    board = generateBoard();
+    generateBombs(board);
+    addAdjacentBombsNumber(board);
+    createCovers(board);
 };
 
 const ROWS = 9;
@@ -17,24 +17,24 @@ const COLS = 9;
 const NUMBER_OF_BOMBS = 10;
 
 const numberColorMap = {
-  1: "blue",
-  2: "green",
-  3: "red",
+    1: "blue",
+    2: "green",
+    3: "red",
 };
 
 // Generate board
 function generateBoard() {
-  const cells = [];
+    const cells = [];
 
-  for (let i = 0; i < ROWS; i++) {
-    const row = [];
-    for (let j = 0; j < COLS; j++) {
-      row.push(Cell([i, j]));
+    for (let i = 0; i < ROWS; i++) {
+        const row = [];
+        for (let j = 0; j < COLS; j++) {
+            row.push(Cell([i, j]));
+        }
+        cells.push(row);
     }
-    cells.push(row);
-  }
 
-  return cells;
+    return cells;
 }
 
 let board = generateBoard();
@@ -43,121 +43,123 @@ addAdjacentBombsNumber(board);
 
 //  Generate bombs at random
 function generateBombs(board) {
-  const bombCoords = [];
-  for (let i = 0; i < NUMBER_OF_BOMBS; i++) {
-    let randomNum = Math.floor(Math.random() * (ROWS * COLS));
-    let x = Math.floor(randomNum / COLS);
-    let y = randomNum % COLS;
-    bombCoords.push([x, y]);
-  }
-  //  Add bombs to board
-  for (let [x, y] of bombCoords) {
-    board[x][y].textContent = "💣";
-  }
+    const bombCoords = [];
+    for (let i = 0; i < NUMBER_OF_BOMBS; i++) {
+        let randomNum = Math.floor(Math.random() * (ROWS * COLS));
+        let x = Math.floor(randomNum / COLS);
+        let y = randomNum % COLS;
+        bombCoords.push([x, y]);
+    }
+    //  Add bombs to board
+    for (let [x, y] of bombCoords) {
+        board[x][y].textContent = "💣";
+    }
 }
 
 // Check if neighboor is in bounds
 function isInBounds([row, col]) {
-  return row < 0 || col < 0 || row > ROWS - 1 || col > COLS - 1 ? false : true;
+    return row < 0 || col < 0 || row > ROWS - 1 || col > COLS - 1
+        ? false
+        : true;
 }
 
 //  Calculate neighbors
 function getNeighbors(coord) {
-  let [row, col] = coord;
+    let [row, col] = coord;
 
-  let neighbors = [
-    [row - 1, col - 1],
-    [row - 1, col],
-    [row - 1, col + 1],
-    [row, col - 1],
-    [row, col + 1],
-    [row + 1, col - 1],
-    [row + 1, col],
-    [row + 1, col + 1],
-  ];
+    let neighbors = [
+        [row - 1, col - 1],
+        [row - 1, col],
+        [row - 1, col + 1],
+        [row, col - 1],
+        [row, col + 1],
+        [row + 1, col - 1],
+        [row + 1, col],
+        [row + 1, col + 1],
+    ];
 
-  return neighbors.filter(isInBounds);
+    return neighbors.filter(isInBounds);
 }
 
 // Calculate number of adjacent bombs
 function numberOfAdjacentBombs([row, col]) {
-  let bombs = 0;
-  let neighbors = getNeighbors([row, col]);
-  for (let [x, y] of neighbors) {
-    if (board[x][y].textContent == "💣") bombs++;
-  }
-  return bombs;
+    let bombs = 0;
+    let neighbors = getNeighbors([row, col]);
+    for (let [x, y] of neighbors) {
+        if (board[x][y].textContent == "💣") bombs++;
+    }
+    return bombs;
 }
 
 function addAdjacentBombsNumber(board) {
-  //  Modify board to include # of adjacent bombs
-  for (let k = 0; k < board.length; k++) {
-    let row = board[k];
-    for (let l = 0; l < row.length; l++) {
-      let numBombs = numberOfAdjacentBombs([k, l]);
-      let cell = board[k][l];
-      let isBomb = cell.textContent == "💣";
-      cell.textContent = isBomb ? "💣" : numBombs > 0 ? numBombs : "";
-      cell.style.color = numberColorMap[cell.textContent] || "black";
+    //  Modify board to include # of adjacent bombs
+    for (let k = 0; k < board.length; k++) {
+        let row = board[k];
+        for (let l = 0; l < row.length; l++) {
+            let numBombs = numberOfAdjacentBombs([k, l]);
+            let cell = board[k][l];
+            let isBomb = cell.textContent == "💣";
+            cell.textContent = isBomb ? "💣" : numBombs > 0 ? numBombs : "";
+            cell.style.color = numberColorMap[cell.textContent] || "black";
+        }
     }
-  }
 }
 
 function revealCell(cell) {
-  cell.children[0].style.visibility = "hidden";
-  if (cell.textContent == "💣") {
-    revealBoard(board);
-    resetBtnElement.textContent = "😵";
-    cell.style.background = "red";
-  }
-  if (cell.textContent == "") propagateReveal(cell, new Set());
+    cell.children[0].style.visibility = "hidden";
+    if (cell.textContent == "💣") {
+        revealBoard(board);
+        resetBtnElement.textContent = "😵";
+        cell.style.background = "red";
+    }
+    if (cell.textContent == "") propagateReveal(cell, new Set());
 }
 
 function coordToKey([row, col]) {
-  return `${row}-${col}`;
+    return `${row}-${col}`;
 }
 
 function propagateReveal(cell, visited) {
-  if (cell.textContent == "") {
-    cell.children[0].style.visibility = "hidden";
-    visited.add(cell.key);
-    for (let [x, y] of getNeighbors(cell.coord)) {
-      if (!visited.has(coordToKey([x, y]))) {
-        propagateReveal(board[x][y], visited);
-      }
+    if (cell.textContent == "") {
+        cell.children[0].style.visibility = "hidden";
+        visited.add(cell.key);
+        for (let [x, y] of getNeighbors(cell.coord)) {
+            if (!visited.has(coordToKey([x, y]))) {
+                propagateReveal(board[x][y], visited);
+            }
+        }
     }
-  }
 }
 
 function Cell([row, col]) {
-  const cell = document.createElement("div");
-  cell.className = "cell";
-  cell.key = `${row}-${col}`;
-  cell.coord = [row, col];
+    const cell = document.createElement("div");
+    cell.className = "cell";
+    cell.key = `${row}-${col}`;
+    cell.coord = [row, col];
 
-  boardElement.appendChild(cell);
+    boardElement.appendChild(cell);
 
-  return cell;
+    return cell;
 }
 
 function createCovers(board) {
-  for (let row of board) {
-    for (let column of row) {
-      const cover = document.createElement("button");
-      cover.className = "cover";
-      cover.onclick = (_) => revealCell(column);
+    for (let row of board) {
+        for (let column of row) {
+            const cover = document.createElement("button");
+            cover.className = "cover";
+            cover.onclick = (_) => revealCell(column);
 
-      column.appendChild(cover);
+            column.appendChild(cover);
+        }
     }
-  }
 }
 
 createCovers(board);
 
 function revealBoard(board) {
-  for (let row of board) {
-    for (let column of row) {
-      column.children[0].style.visibility = "hidden";
+    for (let row of board) {
+        for (let column of row) {
+            column.children[0].style.visibility = "hidden";
+        }
     }
-  }
 }
